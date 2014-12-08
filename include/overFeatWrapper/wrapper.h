55a8b6e5f37c25d2fcf6800fd227d9724271a8de
nradwan <nradwan7@gmail.com>
2014-12-08 13:41:10 +0100

    test

--- include/overFeatWrapper/wrapper.h
+++ include/overFeatWrapper/wrapper.h
@@ -4,7 +4,6 @@
 #include "overfeat.hpp"
 #include "tools/cv2TH.hpp"
 #include <signal.h>
-#include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <assert.h>
 #include "sensor_msgs/Image.h"
@@ -16,12 +15,47 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <ros/ros.h>
+#include <Eigen/Core>
+//#include <pcl/ros/conversions.h>
+//#include <pcl/search/kdtree.h>
+//#include <pcl/search/search.h>
+/*
+#include <pcl/search/pcl_search.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/pca.h>
+#include <pcl/common/eigen.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/feature.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/registration/ia_ransac.h>
+#include <pcl/sample_consensus/sac_model.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/sample_consensus/sac.h>
+#include <pcl/registration/ia_ransac.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+*/
+//#include <pcl_conversions/pcl_conversions.h>
 
 namespace enc = sensor_msgs::image_encodings;
 using namespace cv;
 
 class Wrapper{
 	private:
+		//typedef pcl::PointXYZRGB PCLPoint;
+		//typedef pcl::PointCloud<PCLPoint> PCLPointCloud;
 		ros::NodeHandle node_handle;
 		std::string kinect_topic;
 		std::string classification_topic;
@@ -36,15 +70,25 @@ class Wrapper{
 		ros::ServiceServer classify_server;
 		VideoCapture* cam;
 		Mat_<Vec3b> getCameraFrame_tmp;
+		double clustering_tolerance_;
+		int cluster_min_size_;
+  		int cluster_max_size_;
 		
 		void init();
 		void subscribeToKinect();
 		void unsubscribeFromKinect();
-		void publishClassification(const sensor_msgs::ImageConstPtr &msg);
+		//void publishClassification(const sensor_msgs::PointCloud2::ConstPtr &msg);
+		//void filterPointcloud(PCLPointCloud::Ptr& original_pc, PCLPointCloud::Ptr& objects_pointcloud, PCLPointCloud::Ptr& table_pointcloud);
+		//void clusterPointcloud(PCLPointCloud::Ptr& cloud, vector<pcl::PointIndices>& cluster_indices);
+		//void computeCentroid(const PCLPointCloud& cloud, Eigen::Vector3f& centroid);
+		std::vector<std::pair<std::string, float> > overFeatCallBack(sensor_msgs::Image::ConstPtr& img_msg, double tp_left_x, 
+			double tp_left_y, double width);
 		
 	public:
 		Wrapper(ros::NodeHandle nh);
 		~Wrapper();
 		bool handleClassificationServiceCall(overFeatWrapper::overFeat::Request& req, overFeatWrapper::overFeat::Response& res);
+		
+
 
 };
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -22,6 +22,9 @@ void Wrapper::init(){
 	num_classes = 10;
 	threshold = 0.0;
 	cam = NULL;
+	clustering_tolerance_ = 0.025;
+	cluster_min_size_ =  50;
+  	cluster_max_size_ = 15000;
 	//announce classification service
 	classify_server = node_handle.advertiseService(classification_topic, &Wrapper::handleClassificationServiceCall, this);
 	ROS_INFO("Announced service: %s", classification_topic.c_str());
@@ -47,7 +50,7 @@ bool Wrapper::handleClassificationServiceCall(overFeatWrapper::overFeat::Request
 }
 
 void Wrapper::subscribeToKinect(){
-	kinect_subscriber = node_handle.subscribe(kinect_topic, 1, &Wrapper::publishClassification, this);
+	//kinect_subscriber = node_handle.subscribe(kinect_topic, 1, &Wrapper::publishClassification, this);
 	ROS_INFO("Subscribed to: %s", kinect_topic.c_str());
 }
 
@@ -56,16 +59,68 @@ void Wrapper::unsubscribeFromKinect(){
 	ROS_WARN("UNsubscribed from: %s", kinect_topic.c_str());
 }
 
-void Wrapper::publishClassification(const sensor_msgs::ImageConstPtr &msg){
-	ROS_INFO("Inside publishClassification");
-	//convert the kinect image to cv image
+/*void Wrapper::filterPointcloud(PCLPointCloud::Ptr& original_pc, PCLPointCloud::Ptr& objects_pointcloud, PCLPointCloud::Ptr& table_pointcloud){
+	double plane_thresh = 0.03;
+	pcl::ModelCoefficients coefficients;
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+	// Create the segmentation object
+	pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setDistanceThreshold(plane_thresh);
+	seg.setInputCloud(original_pc);
+	seg.segment(*inliers, coefficients);
+	
+	pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+	// Extract table
+	extract.setInputCloud(original_pc);
+	extract.setIndices(inliers);
+	extract.setNegative(false);
+	table_pointcloud->clear();
+	extract.filter(*table_pointcloud);
+
+	// Extract objects
+	extract.setInputCloud(original_pc);
+	extract.setIndices(inliers);
+	extract.setNegative(true);
+	objects_pointcloud->clear();
+	extract.filter(*objects_pointcloud);
+}
+
+void Wrapper::clusterPointcloud(PCLPointCloud::Ptr& cloud, std::vector<pcl::PointIndices>& cluster_indices){
+	// KdTree object for the search method of the extraction
+	pcl::search::KdTree<PCLPoint>::Ptr kdtree_cl(new pcl::search::KdTree<PCLPoint>());
+	kdtree_cl->setInputCloud(cloud);
+	pcl::EuclideanClusterExtraction<PCLPoint> ec;
+	ec.setClusterTolerance(clustering_tolerance_);
+	ec.setMinClusterSize(cluster_min_size_);
+	ec.setMaxClusterSize(cluster_max_size_);
+	ec.setSearchMethod(kdtree_cl);
+	ec.setInputCloud(cloud);
+	ec.extract(cluster_indices);
+}
+*/
+// Computes the object centroid based on the pointcloud
+/*void Wrapper::computeCentroid(const PCLPointCloud& cloud, Eigen::Vector3f& centroid){
+	Eigen::Vector4f centr;
+	pcl::compute3DCentroid(cloud, centr);
+	centroid[0] = centr[0];
+	centroid[1] = centr[1];
+	centroid[2] = centr[2];
+}*/
+
+std::vector<std::pair<std::string, float> > Wrapper::overFeatCallBack(sensor_msgs::Image::ConstPtr& img_msg, double tp_left_x, 
+			double tp_left_y, double width){
+	
+	std::vector<std::pair<std::string, float> > empty_res;
 	cv_bridge::CvImagePtr cv_ptr;
 	try{
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(img_msg, enc::BGR8);
     }
     catch (cv_bridge::Exception& e){
       ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+      return empty_res;
     }
    //imwrite("/home/noha/Documents/Hiwi/bowl.jpg", cv_ptr->image);
 	try {
@@ -76,11 +131,9 @@ void Wrapper::publishClassification(const sensor_msgs::ImageConstPtr &msg){
     THTensor* probas = THTensor_(new)();
     ROS_INFO("Finished initialization");
     
-    Mat_<Vec3b> cm_image =  imread("/home/noha/Documents/Hiwi/bowl.jpg");//cv_ptr->image;
+    Mat_<Vec3b> cm_image = cv_ptr->image;//imread("/home/noha/Documents/Hiwi/bowl.jpg");
     //resize the image
-    int cam_w = cm_image.size().width;
-    int cam_h = cm_image.size().height;
-    Rect myROI(cam_w/2 - cam_h/2, 0, cam_h, cam_h);
+    Rect myROI(tp_left_x, tp_left_y, width, width);
     Mat_<Vec3b> im;
   	cm_image = cm_image(myROI);
   	resize(cm_image, im, Size(231, 231), 0, 0, INTER_CUBIC);
@@ -106,34 +159,84 @@ void Wrapper::publishClassification(const sensor_msgs::ImageConstPtr &msg){
 
 	ROS_INFO("Finished converting output to probabilities");
 	
-	// Create msg and publish the output in it
+	// get top classes
 	std::vector<std::pair<std::string, float> > top_classes = overfeat::get_top_classes(probas, num_classes);
-	classification_pub = node_handle.advertise<overFeatWrapper::detectedObjArray> (pub_topic, 1);
-	ROS_INFO("Advertised topic: %s", pub_topic.c_str());
-	overFeatWrapper::detectedObjArray top_classes_msg;
-	for(std::vector<std::pair<std::string, float> >::iterator it = top_classes.begin();
-			it != top_classes.end(); ++it){
-		//skip objects with probability less than a certain threshold
-		//if(it->second < threshold)
-			//continue;
-		overFeatWrapper::detectedObj curr_obj;
-		curr_obj.obj_name = it->first;
-		curr_obj.prob = it->second;
-		top_classes_msg.detected_classes.push_back(curr_obj);
-	}
-	classification_pub.publish(top_classes_msg);
-	//displayWithConf(input, top_classes);
 
     THTensor_(free)(input);
     THTensor_(free)(probas);
     overfeat::free();
-
+	return top_classes;
+	
   } catch (cv::Exception & e) {
     ROS_INFO("CV Error!");
    	//killed(0);
-    return;
-  }
+    return empty_res;
+  }		
 }
+
+//void Wrapper::publishClassification(const sensor_msgs::PointCloud2::ConstPtr &msg){
+	// Extract table plane. Return a point cloud of the table and one of the objects.
+	/*PCLPointCloud::Ptr objs_cloud(new PCLPointCloud);
+	PCLPointCloud::Ptr table_cloud(new PCLPointCloud);
+	PCLPointCloud::Ptr n_original_pc(new pcl::PCLPointCloud2());
+	pcl::fromROSMsg(*msg, *n_original_pc);
+	filterPointcloud(n_original_pc, objs_cloud, table_cloud);
+	
+	//Cluster the objects
+	std::vector<pcl::PointIndices> cluster_indices;
+	std::vector<pcl::PointIndices>::const_iterator it;
+	clusterPointcloud(objs_cloud, cluster_indices);
+	std::vector<PCLPointCloud::Ptr> new_clusters; // collect new clusters
+	/*std::vector<Eigen::Vector3f> new_cluster_centroids; // collect new cluster centroids
+	for (it = cluster_indices.begin(); it != cluster_indices.end(); ++it) {
+		PCLPointCloud::Ptr single_cluster(new PCLPointCloud);
+		single_cluster->width = it->indices.size();
+		single_cluster->height = 1;
+		single_cluster->points.reserve(it->indices.size());
+		std::vector<int>::const_iterator pit;
+		for (pit = it->indices.begin(); pit != it->indices.end(); ++pit) {
+			PCLPoint& pnt = objs_cloud->points[*pit];
+			single_cluster->points.push_back(pnt);
+		}
+    	new_clusters.push_back(single_cluster);
+		// Compute centroid
+ 		Eigen::Vector3f new_centr;
+		computeCentroid(*single_cluster, new_centr);
+		new_cluster_centroids.push_back(new_centr);
+	}
+	
+	//Get the classification of each clustered object
+	overFeatWrapper::detectedObjArray top_classes_msg;
+	std::vector<PCLPointCloud::Ptr>::iterator clust_it;
+	/*std::vector<Eigen::Vector3f>::iterator centr_it = new_cluster_centroids.begin();
+	int idx = 0;
+	for(clust_it = new_clusters.begin(); clust_it != new_clusters.end(); ++clust_it){
+		//convert from pointcloud to image
+		sensor_msgs::Image::ConstPtr img_msg;
+		pcl::toROSMsg(**clust_it, *img_msg);
+		//get coordinates for cropping
+		Eigen::Vector3f centr = *centr_it; 
+		double tp_left_x = centr(0) - (*clust_it)->points.size();
+		double tp_left_y = centr(1) - (*clust_it)->points.size();
+		//call classification method
+		std::vector<std::pair<std::string, float> > top_classes = overFeatCallBack(img_msg, tp_left_x, tp_left_y, (*clust_it)->points.size());
+		//concatenate result to top_classes_msg
+		for(std::vector<std::pair<std::string, float> >::iterator it = top_classes.begin();
+			it != top_classes.end(); ++it){
+			//skip objects with probability less than a certain threshold
+			//if(it->second < threshold)
+				//continue;
+			overFeatWrapper::detectedObj curr_obj;
+			curr_obj.obj_name = it->first;
+			curr_obj.prob = it->second;
+			curr_obj.cluster_idx = idx;
+			top_classes_msg.detected_classes.push_back(curr_obj);
+		}
+		++idx;
+		++centr_it;
+	}
+	classification_pub.publish(top_classes_msg);*/
+//}
 
 
 int main(int argc, char** argv){
